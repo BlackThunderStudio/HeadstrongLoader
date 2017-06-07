72730933e53928d0b4acc0ea10f3dd31af6296b1
Rajmund Staniek <rajmund.st@gmail.com>
2017-06-07 12:48:54 +0200

    some stuff

--- src/com/headstrongpro/desktopLoader/view/UpdaterView.java
+++ src/com/headstrongpro/desktopLoader/view/UpdaterView.java
@@ -1,12 +1,23 @@
 package com.headstrongpro.desktopLoader.view;
 
+import com.headstrongpro.desktopLoader.Main;
 import com.jfoenix.controls.JFXProgressBar;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * HeadstrongLoader
@@ -20,10 +31,55 @@ public class UpdaterView implements Initializable {
     @FXML
     public Label infoLabel;
 
+    private static final String UPDATE_PATH = "http://remix1436.ct8.pl/resources/headstrong/version.json";
+    private static final String UPDATE_ROOT = "desktop";
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         infoLabel.setText("Checking for updates...");
         progressBar.setProgress(-1.0f);
         progressBar.setVisible(true);
+
+        checkForUpdates.stateProperty().addListener(((observable, oldValue, newValue) -> {
+            if(newValue.equals(Worker.State.SUCCEEDED)){
+                progressBar.setVisible(false);
+            }
+        }));
+
+        checkForUpdates.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            if(newValue != null){
+                if(newValue){
+                    infoLabel.setText("Update found! Downloading...");
+                } else {
+                    infoLabel.setText("");
+                    System.out.println("Starting the main application");
+                    try {
+                        openHeadstrongManager();
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Platform.exit();
+                }
+            }
+        }));
+
+        new Thread(checkForUpdates).start();
+    }
+
+    private Task<Boolean> checkForUpdates = new Task<Boolean>() {
+        @Override
+        protected Boolean call() throws Exception {
+            JSONParser parser = new JSONParser();
+            JSONObject rawJson = (JSONObject) parser.parse(new BufferedReader(new InputStreamReader(new URL(UPDATE_PATH).openStream())));
+            rawJson = (JSONObject)rawJson.get(UPDATE_ROOT);
+            String version = (String)rawJson.get("version");
+            System.out.println("server version: " + version);
+            Thread.sleep(1000);
+            return false;
+        }
+    };
+
+    private void openHeadstrongManager() throws IOException, InterruptedException {
+        Process process = Runtime.getRuntime().exec(new String[]{"java", "-jar", "desktop_manager.jar"});
     }
 }
